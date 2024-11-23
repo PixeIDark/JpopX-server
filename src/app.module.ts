6471ba22671a67b0e9efed6e29a4ac2105559a0b
Darkthos <kiki7951@naver.com>
2024-11-23 18:45:55 +0900

    제발 성공해줘

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
@@ -16,5 +17,6 @@ import { DatabaseModule } from './database/database.module';
     UsersModule,
   ],
   controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
