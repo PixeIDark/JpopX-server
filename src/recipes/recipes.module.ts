9929606e72899cc391ebbeb0366b6aa33af8906d
Darkthos <kiki7951@naver.com>
2024-12-18 17:01:11 +0900

    빌드 오류 수정

--- src/recipes/recipes.module.ts
+++ src/recipes/recipes.module.ts
@@ -2,9 +2,20 @@ import { Module } from '@nestjs/common';
 import { RecipesController } from './recipes.controller';
 import { RecipesService } from './recipes.service';
 import { DatabaseModule } from '../database/database.module';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [
+    DatabaseModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get('JWT_SECRET'),
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [RecipesController],
   providers: [RecipesService],
 })
