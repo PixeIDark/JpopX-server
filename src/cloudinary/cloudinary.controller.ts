d715e3efa254fbb347f6608dbd916fac908e65b3
Darkthos <kiki7951@naver.com>
2024-12-21 20:30:15 +0900

    이미지 없으면 경고뜸

--- src/cloudinary/cloudinary.controller.ts
+++ src/cloudinary/cloudinary.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, UploadedFile, UploadedFiles, UseInterceptors } from '@nestjs/common';
+import { BadRequestException, Controller, Post, UploadedFile, UploadedFiles, UseInterceptors } from '@nestjs/common';
 import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
 import { CloudinaryService } from './cloudinary.service';
 import { memoryStorage } from 'multer';  // 추가
@@ -13,6 +13,9 @@ export class CloudinaryController {
     storage: memoryStorage(),
   }))
   async uploadSingle(@UploadedFile() file: Express.Multer.File) {
+    if (!file) {
+      throw new BadRequestException('이미지 파일을 업로드 해주세요.');
+    }
     const url = await this.cloudinaryService.uploadImage(file);
     return { url };
   }
@@ -22,6 +25,9 @@ export class CloudinaryController {
     storage: memoryStorage(),
   }))
   async uploadMultiple(@UploadedFiles() files: Express.Multer.File[]) {
+    if (!files || files.length === 0) {
+      throw new BadRequestException('하나 이상의 이미지 파일을 업로드 해주세요.');
+    }
     const urls = await this.cloudinaryService.uploadMultipleImages(files);
     return { urls };
   }
