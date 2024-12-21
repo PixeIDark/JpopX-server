4ae6203402f1cd11fd2ee8c45d9558655a2db941
Darkthos <kiki7951@naver.com>
2024-12-21 20:13:50 +0900

    버퍼 저장

--- src/cloudinary/cloudinary.controller.ts
+++ src/cloudinary/cloudinary.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Post, UploadedFile, UploadedFiles, UseInterceptors } from '@nestjs/common';
 import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
 import { CloudinaryService } from './cloudinary.service';
+import { memoryStorage } from 'multer';  // 추가
 
 @Controller('upload')
 export class CloudinaryController {
@@ -8,14 +9,18 @@ export class CloudinaryController {
   }
 
   @Post('single')
-  @UseInterceptors(FileInterceptor('file'))
+  @UseInterceptors(FileInterceptor('file', {
+    storage: memoryStorage(),
+  }))
   async uploadSingle(@UploadedFile() file: Express.Multer.File) {
     const url = await this.cloudinaryService.uploadImage(file);
     return { url };
   }
 
   @Post('multiple')
-  @UseInterceptors(FilesInterceptor('files'))
+  @UseInterceptors(FilesInterceptor('files', 50, {
+    storage: memoryStorage(),
+  }))
   async uploadMultiple(@UploadedFiles() files: Express.Multer.File[]) {
     const urls = await this.cloudinaryService.uploadMultipleImages(files);
     return { urls };
