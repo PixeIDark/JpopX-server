1c9a446c5fcf9228a578a011f4e4eedcbd68e35a
Darkthos <kiki7951@naver.com>
2024-12-21 20:08:22 +0900

    빌드오류 5

--- src/cloudinary/cloudinary.service.ts
+++ src/cloudinary/cloudinary.service.ts
@@ -1,22 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import { v2 as cloudinary } from 'cloudinary';
 import { Readable } from 'stream';
-import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class CloudinaryService {
-  constructor(private configService: ConfigService) {
+  constructor() {
     cloudinary.config({
-      cloud_name: this.configService.get('CLOUDINARY_CLOUD_NAME'),
-      api_key: this.configService.get('CLOUDINARY_API_KEY'),
-      api_secret: this.configService.get('CLOUDINARY_API_SECRET'),
+      cloud_name: 'dqbckjofm',
+      api_key: '616963312383764',
+      api_secret: 'ooen9a5UCbGvNaKWWIgVIWCSbVs',
     });
   }
 
   async uploadImage(file: Express.Multer.File): Promise<string> {
     return new Promise((resolve, reject) => {
       const uploadStream = cloudinary.uploader.upload_stream(
-        { folder: process.env.CLOUDINARY_FOLDER },
+        { folder: 'minchelin' },
         (error, result) => {
           if (error) return reject(error);
           resolve(result.secure_url);
